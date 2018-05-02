f9f2f86537f3fe26e3a7f8a7a4e46c2b0be5a990
Ramadan Saputra <rarasta27@gmail.com>
2018-05-02 23:16:04 +0700

    config.php
    => perbaikan typo

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,5 @@
 <?php
 	// config base url
-	// define('BASE_URL', 'http://localhost/latihan/framework_oop/'); // isi path dari web
 	define('BASE_URL', 'http://localhost/cv_69design_build/'); // isi path dari web
 	define('SITE_URL', BASE_URL.'index.php/'); // hilangkan index.php atau komentari SITE_URL jika sudah memakai .htaccess
 	define('DEFAULT_CONTROLLER', 'home'); // default controller yg diakses pertama kali
