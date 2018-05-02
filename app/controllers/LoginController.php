055c8eb06182e5ecff2668e0b7eee6322be97f67
Ramadan Saputra <rarasta27@gmail.com>
2018-05-02 23:24:01 +0700

    LoginController.php
    => perapihan code
    => penyederhanaan code
    => penambahan komentar pendukung di setiap method

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -6,29 +6,65 @@
 		protected $username = 'ABCD';
 		protected $password = 'ABCD';
 		protected $token;
-		// protected $logout = "ABCD";
 
 		public function __construct(){
-			
+			$this->auth();
 		}
 
+		/**
+		* fungsi index untuk akses utama controller login
+		*/
 		public function index(){
-			$this->auth();
-
 			$jenis = isset($_POST['jenis']) ? $_POST['jenis'] : false;
-			if(!$jenis){
-				if($this->auth->isLogin()) $this->redirect(BASE_URL);
-			}
 
 			// cek jenis login
-			if($jenis) $this->loginMobile();
-			else{
-				if($_SERVER['REQUEST_METHOD'] == "POST") $this->loginSistem();
-				else $this->view('login');
+			if($jenis) $this->loginMobile(); // jika mobile
+			else{ // jika sistem
+				if($this->auth->isLogin()) $this->redirect(BASE_URL); // jika sudah login, tidak bisa akses
+				else{ // jika belum login
+					if($_SERVER['REQUEST_METHOD'] == "POST") $this->loginSistem(); // jika request post login
+					else $this->view('login'); // jika bukan, atau hanya menampilkan halaman login
+				}
 			}
 		}
 
+		/**
+		* fungsi login untuk sistem
+		* pengecekan user dan password berdasarkan jenis user
+		* pemberian hak akses berdasarkan level
+		* set session default
+		*/
+		private function loginSistem(){
+			$user = isset($_POST['user']) ? $_POST['user'] : false;
+			$pass = isset($_POST['pass']) ? $_POST['pass'] : false;
+
+			// get username
+
+			// cek username
+
+			// cek jenis
+
+			if(($user === $this->username) && ($pass === $this->password)){
+				$_SESSION['sess_login'] = true;
+				$_SESSION['sess_locksreen'] = false;
+
+				$this->redirect(BASE_URL);
+			}
+			else{
+				
+			}
+		}
+
+		/**
+		* Fungsi login untuk mobile
+		* jika login baru, maka akan generate token baru dan disimpan ke db
+		* token baru yg sudah digenerate akan di hash md5 dan bycrpt ke db
+		* sedangkan yg dikirim ke mobile adalah hash md5
+		* return fungsi berupa json
+		*/
 		private function loginMobile(){
+			$this->auth->mobileOnly();
+
 			// validasi pengguna
 			$user = isset($_POST['user']) ? $_POST['user'] : false;
 			$pass = isset($_POST['pass']) ? $_POST['pass'] : false;
@@ -53,24 +89,19 @@
 			echo json_encode($output);
 		}
 
-		private function loginSistem(){
-			$user = isset($_POST['user']) ? $_POST['user'] : false;
-			$pass = isset($_POST['pass']) ? $_POST['pass'] : false;
+		/**
+		* Fungsi lockscreen
+		* set ulang session login dan session lockscreen saja
+		*/
+		private function lockscreen(){
 
-			if(($user === $this->username) && ($pass === $this->password)){
-				// set session
-				echo "Berhasil Masuk Sistem";
-
-				$_SESSION['sess_login'] = true;
-				$_SESSION['sess_locksreen'] = false;
-			}
-			else{
-				echo "Gagal Masuk Sistem";
-			}
 		}
 
+		/**
+		* Fungsi logout
+		* menghapus semua session yang ada
+		*/
 		public function logout(){
-			// session_start();
 			session_unset();
 			session_destroy();
 
