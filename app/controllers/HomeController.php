de3b058ba8d8ef8216c944e3c096ed56ff243354
Ramadan Saputra <rarasta27@gmail.com>
2018-05-02 23:35:32 +0700

    Modul Beranda
    => HomeController.php
    => views/beranda/kas_kecil.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,13 +1,86 @@
 <?php
 	// namespace app\controllers;
 
-	class Home{
+	class Home extends Controller{
 
 		public function __construct(){
-			echo "Selamat Datang di Class Home <br>";
+			$this->auth();
+			$this->auth->cekAuth();
 		}
 
+		/**
+		* 
+		*/
 		public function index(){
+			// cek jenis user
 
+			// arahkan ke beranda masing-masing
+			$this->beranda_kasKecil();
+		}
+
+		/**
+		*
+		*/
+		private function beranda_owner(){
+			// config css-js
+			$css = array();
+			$js = array();
+
+			$config = array(
+				'title' => array(
+					'main' => 'Beranda',
+					'sub' => 'Dashboard Owner',
+				),
+				'css' => $css,
+				'js' => $js,
+			);
+
+			$data = null;
+
+			$this->layout('beranda/owner', $config, $data);
+		}
+
+		/**
+		*
+		*/
+		private function beranda_kasBesar(){
+			// config css-js
+			$css = array();
+			$js = array();
+
+			$config = array(
+				'title' => array(
+					'main' => 'Beranda',
+					'sub' => 'Dashboard Kas Besar',
+				),
+				'css' => $css,
+				'js' => $js,
+			);
+
+			$data = null;
+
+			$this->layout('beranda/kas_besar', $config, $data);
+		}
+
+		/**
+		*
+		*/
+		private function beranda_kasKecil(){
+			// config css-js
+			$css = array();
+			$js = array();
+
+			$config = array(
+				'title' => array(
+					'main' => 'Beranda',
+					'sub' => 'Dashboard Kas Kecil',
+				),
+				'css' => $css,
+				'js' => $js,
+			);
+
+			$data = null;
+
+			$this->layout('beranda/kas_kecil', $config, $data);
 		}
 	}
