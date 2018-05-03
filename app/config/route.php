15ebbe759ca4efc4c4766d7df6c0e07fec615f8c
Ramadan Saputra <rarasta27@gmail.com>
2018-05-03 08:16:12 +0700

    route.php
    => perbaikan setting request untuk get controller
    => jika nama controller yg diakses panjang, gunakan dash ('-') sebagai penyambung katanya.
    => penamaan class wajib disambung underscore ('_') jika terdapat lebih dari satu kata

--- app/config/route.php
+++ app/config/route.php
@@ -24,21 +24,31 @@
 		*/
 		public function getController(){
 			$uri = explode('/', $this->__request);
-			$class = isset($uri[0]) && ($uri[0] != "") ? strtolower(ucfirst($uri[0])) : DEFAULT_CONTROLLER; // class
+			$class = isset($uri[0]) && ($uri[0] != "") ? strtolower($uri[0]) : DEFAULT_CONTROLLER; // class
 			$method = isset($uri[1]) ? strtolower($uri[1]) : 'index';	// method
-			$param = isset($uri[2]) ? $uri[2] : false;	// param
+			$param = isset($uri[2]) ? strtolower($uri[2]) : false;	// param
+
+			// explode request untuk url cantik
+			$class = str_replace('_', ' ', $class);
+			$method = str_replace('_', ' ', $method);
+
+			$tempClass = explode('-', $class);
+			$tempMethod = explode('-', $method);
+
+			$newClass = ucfirst(implode('_', $tempClass));
+			$newMethod = implode('_', $tempMethod);
 
 			// set request controller - class
-			$this->__controller = ROOT.DS.'app'.DS.'controllers'.DS.$class."Controller.php";
+			$this->__controller = ROOT.DS.'app'.DS.'controllers'.DS.$newClass."Controller.php";
 
 			// cek file controller
 			if(file_exists($this->__controller)){
 				// load controller dan class
 				require_once $this->__controller;
-				$obj = new $class();
+				$obj = new $newClass();
 
-				if(method_exists($obj, $method)){
-					$obj->$method($param);
+				if(method_exists($obj, $newMethod)){
+					$obj->$newMethod($param);
 					// call_user_func_array(array($obj, $method), array());
 				}
 				else die($this->error('403')); // method tidak tersedia	
