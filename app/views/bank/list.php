de61fa20f925b1e7e16fb470289cf3b795785862
Ramadan Saputra <rarasta27@gmail.com>
2018-05-19 11:28:28 +0700

    Modul Bank

--- app/views/bank/list.php
+++ app/views/bank/list.php
@@ -46,7 +46,7 @@
 								<table id="bankTable" class="table table-bordered table-hover">
 									<thead>
 										<tr>
-											<th class="text-center">No</th>
+											<th class="text-center" style="width: 35px">No</th>
 											<th class="text-center">Nama</th>
 											<th class="text-center">Saldo</th>
 											<th class="text-center">Status</th>
