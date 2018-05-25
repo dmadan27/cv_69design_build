20633fbb75d49eec2f584de6c854020e2ed0b1f5
Jaka Pratama <jakapratama204@gmail.com>
2018-05-25 11:31:01 +0700

    Modul Operasional Proyek (On Progress)
    => view belum
    => baru controller

--- app/controllers/Operasional_ProyekController.php
+++ app/controllers/Operasional_ProyekController.php
@@ -1,20 +1,58 @@
 <?php
-class Operasional_Proyek extends Crud_modalAbstract{
+Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
+
+class Operasional_Proyek extends Crud_modalsAbstract{
 
 	public function __construct(){
 			$this->auth();
 			$this->auth->cekAuth();
-			$this->model('BankModel');
+			$this->model('Operasional_ProyekModel');
 			$this->helper();
 			$this->validation();
 	}
 
 	public function index(){
-
+		$this->list();
 	}
 
-	public function list(){
+	protected function list(){
+		// set config untuk layouting
+			$css = array(
+				'assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css',
+			);
+			$js = array(
+				'assets/bower_components/datatables.net/js/jquery.dataTables.min.js', 
+				'assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js',
+				'app/views/operasional_proyek/js/initList.js',
+				'app/views/operasional_proyek/js/initForm.js',
+			);
 
+			$config = array(
+				'title' => array(
+					'main' => 'Data Operasional Proyek',
+					'sub' => 'List Semua Data Operasional Proyek',
+				),
+				'css' => $css,
+				'js' => $js,
+			);
+			
+			// // set token
+			// $_SESSION['token_bank'] = array(
+			// 	'list' => md5($this->auth->getToken()),
+			// 	'add' => md5($this->auth->getToken()),
+			// );
+
+			// $this->token = array(
+			// 	'list' => password_hash($_SESSION['token_bank']['list'], PASSWORD_BCRYPT),
+			// 	'add' => password_hash($_SESSION['token_bank']['add'], PASSWORD_BCRYPT),	
+			// );
+
+			// $data = array(
+			// 	'token_list' => $this->token['list'],
+			// 	'token_add' => $this->token['add'],
+			// );
+
+			$this->layout('operasional_proyek/list', $config);
 	}
 
 	public function action_add(){
@@ -22,10 +60,67 @@ class Operasional_Proyek extends Crud_modalAbstract{
 	}
 
 	public function get_list(){
+		// $token = isset($_POST['token_list']) ? $_POST['token_list'] : false;
+			
+			// cek token
+			// $this->auth->cekToken($_SESSION['token_bank']['list'], $token, 'bank');
+			
+			// config datatable
+			$config_dataTable = array(
+				'tabel' => 'operasional_proyek',
+				'kolomOrder' => array(null, 'id', 'id_proyek', 'tgl', 'nama', 'total', null),
+				'kolomCari' => array('nama', 'tgl',),
+				'orderBy' => array('id' => 'asc'),
+				'kondisi' => false,
+			);
 
+			$dataOperasionalProyek = $this->Opersional_ProyekModel->getAllDataTable($config_dataTable);
+
+			// // set token
+			// $_SESSION['token_bank']['edit'] = md5($this->auth->getToken());
+			// $_SESSION['token_bank']['delete'] = md5($this->auth->getToken());
+			
+			// $this->token = array(
+			// 	'edit' => password_hash($_SESSION['token_bank']['edit'], PASSWORD_BCRYPT),
+			// 	'delete' => password_hash($_SESSION['token_bank']['delete'], PASSWORD_BCRYPT),	
+			// );
+
+			$data = array();
+			$no_urut = $_POST['start'];
+			foreach($dataOperasionalProyek as $row){
+				$no_urut++;
+
+				// $status = ($row['status'] == "AKTIF") ? '<span class="label label-success">'.$row['status'].'</span>' : '<span class="label label-danger">'.$row['status'].'</span>';
+
+				// button aksi
+				$aksiDetail = '<button onclick="getView('."'".$row["id"]."'".')" type="button" class="btn btn-sm btn-info btn-flat" title="Lihat Detail"><i class="fa fa-eye"></i></button>';
+				$aksiEdit = '<button onclick="getEdit('."'".$row["id"]."'".', '."'".$this->token["edit"]."'".')" type="button" class="btn btn-sm btn-success btn-flat" title="Edit Data"><i class="fa fa-pencil"></i></button>';
+				$aksiHapus = '<button onclick="getDelete('."'".$row["id"]."'".', '."'".$this->token["delete"]."'".')" type="button" class="btn btn-sm btn-danger btn-flat" title="Hapus Data"><i class="fa fa-trash"></i></button>';
+				
+				$aksi = '<div class="btn-group">'.$aksiDetail.$aksiEdit.$aksiHapus.'</div>';
+				
+				$dataRow = array();
+				$dataRow[] = $no_urut;
+				$dataRow[] = $row['id'];
+				$dataRow[] = $row['tgl'];
+				$dataRow[] = $row['nama'];
+				$dataRow[] = $row['total'];
+				$dataRow[] = $aksi;
+
+				$data[] = $dataRow;
+			}
+
+			$output = array(
+				'draw' => $_POST['draw'],
+				'recordsTotal' => $this->Opersional_ProyekModel->recordTotal(),
+				'recordsFiltered' => $this->Opersional_ProyekModel->recordFilter(),
+				'data' => $data,
+			);
+
+			echo json_encode($output);	
 	}
 
-	public function edit(){
+	public function edit($id){
 
 	}
 
@@ -33,11 +128,11 @@ class Operasional_Proyek extends Crud_modalAbstract{
 
 	}
 
-	public function detail(){
+	public function detail($id){
 
 	}
 
-	public function delete(){
+	public function delete($id){
 
 	}
 
