9040a6eef142738245f0fbd66abbe6041c397a19
rezzarediza <fajarcahyo15@gmail.com>
2018-05-29 13:22:19 +0700

    perbaikan API proyek (list_pengajuan -> list_proyek)

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,16 +1,16 @@
 <?php
 	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
-	
+
 	/**
-	* 
+	*
 	*/
 	class Api extends Controller{
-		
+
 		protected $status = true;
-		
+
 		/**
-		* 
-		*/		
+		*
+		*/
 		public function __construct(){
 			$this->auth();
 			$this->auth->mobileOnly();
@@ -18,20 +18,20 @@
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function index(){
 			$this->pengajuan();
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function pengajuan(){
-			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;	
+			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;
 
 			$this->model('Pengajuan_sub_kas_kecilModel');
-		
+
 			$dataPengajuan = $this->Pengajuan_sub_kas_kecilModel->getAll_mobile($page);
 			$totalData = $this->Pengajuan_sub_kas_kecilModel->get_recordTotal_mobile();
 			$totalPage = ceil($totalData/10);
@@ -57,13 +57,13 @@
 			$this->model('Pengajuan_sub_kas_kecilModel');
 
 			// generate id pengajuan
-			
-			
+
+
 			// get data saldo
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function action_add_pengajuan(){
 			$data = isset($_POST) ? $_POST : false;
@@ -114,20 +114,20 @@
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function add_laporan(){
 
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function proyek(){
-			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;	
+			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;
 
 			$this->model('ProyekModel');
-		
+
 			$dataProyek = $this->ProyekModel->getAll_mobile($page);
 			$totalData = $this->ProyekModel->get_recordTotal_mobile();
 			$totalPage = ceil($totalData/10);
@@ -135,7 +135,7 @@
 			$next = ($page < $totalPage) ? ($page + 1) : null;
 
 			$output = array(
-				'list_pengajuan' => $dataProyek,
+				'list_proyek' => $dataProyek,
 				'next' => $next,
 				'status' => $this->status,
 				// 'page' => $page,
@@ -147,20 +147,20 @@
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function profil(){
-			
+
 		}
 
 		/**
-		* 
+		*
 		*/
 		public function mutasi(){
-			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;	
+			$page = (isset($_POST['page']) && !empty($_POST['page'])) ? $_POST['page'] : 1;
 
 			$this->model('ProyekModel');
-		
+
 			$dataProyek = $this->ProyekModel->getAll_mobile($page);
 			$totalData = $this->ProyekModel->get_recordTotal_mobile();
 			$totalPage = ceil($totalData/10);
@@ -180,8 +180,8 @@
 		}
 
 		/**
-		* 
+		*
 		*/
-		
+
 
 	}
