3f84e0cb8af78a38af1684a695e5bde39912d4c7
Ramadan Saputra <rarasta27@gmail.com>
2018-07-01 12:41:04 +0700

    Modul Lupa Password

---
+++ app/controllers/Lupa_passwordController.php
@@ -0,0 +1,36 @@
+<?php
+	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
+
+	/**
+	*
+	*/
+	class Lupa_password extends Controller{
+		/**
+		*
+		*/
+		public function __construct(){
+			
+		}
+
+		/**
+		*
+		*/
+		public function index(){
+
+		}
+
+		/**
+		*
+		*/
+		private function lupa_password(){
+
+		}
+
+		/**
+		*
+		*/
+		public function lupa_password_mobile(){
+			
+		}
+
+	}
