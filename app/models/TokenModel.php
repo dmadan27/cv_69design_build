943be93a7960806a0c26ecfeb39bf180b6fdceb1
Ramadan Saputra <rarasta27@gmail.com>
2018-07-02 10:40:37 +0700

    TokenModel
    => perbaikan di beberapa method, dari ubah nama, dan penerapan transaction

--- app/models/TokenModel.php
+++ app/models/TokenModel.php
@@ -19,50 +19,150 @@
 		/**
 		* 
 		*/
-		public function getToken($user){
-			$query = "SELECT t.token, t.tgl_exp FROM token_sub_kas_kecil t ";
-			$query .= "JOIN sub_kas_kecil skc ON skc.id = t.id_sub_kas_kecil ";
-			$query .= "WHERE skc.email = :user";
+		public function getToken_lupa_password($user){
+			$query = "SELECT t.token, t.tgl_exp FROM token_lupa_password t ";
+			$query .= "JOIN user u ON u.username = t.username ";
+			$query .= "WHERE u.username = :user";
 
 			$statement = $this->koneksi->prepare($query);
 			$statement->bindParam(':user', $user);
-			$result = $statement->execute();
+			$statement->execute();
 			$result = $statement->fetch(PDO::FETCH_ASSOC);
 
 			return $result;
 		}
 
 		/**
-		* 
+		*
 		*/
-		public function insert($data){
-			$query = "INSERT INTO token_sub_kas_kecil (id_sub_kas_kecil, token, tgl_buat, tgl_exp) ";
-			$query .= "VALUES (:id_sub_kas_kecil, :token, :tgl_buat, :tgl_exp)";
+		public function setToken_lupa_password($data){
+			try{
+				$this->koneksi->beginTransaction();
+				
+				// hapus token lama
+				$this->delete_lupa_password($data['username']);
 
-			$statement = $this->koneksi->prepare($query);
-			$statement->bindParam(':id_sub_kas_kecil', $data['id_sub_kas_kecil']);
-			$statement->bindParam(':token', $data['token']);
-			$statement->bindParam(':tgl_buat', $data['tgl_buat']);
-			$statement->bindParam(':tgl_exp', $data['tgl_exp']);	
-			
-			$result = $statement->execute();
+				// tambah token baru
+				$this->insert_lupa_password($data);
 
-			return $result;
+				$this->koneksi->commit();
+
+				return true;
+			}
+			catch(PDOException $e){
+				$this->koneksi->rollback();
+				die($e->getMessage());
+				// return false;
+			}
 		}
 
 		/**
 		* 
 		*/
-		public function delete($id){
-			$query = "DELETE FROM token_sub_kas_kecil WHERE id_sub_kas_kecil = :id";
+		private function insert_lupa_password($data){
+			$query = "INSERT INTO token_lupa_password (username, token, tgl_buat, tgl_exp) ";
+			$query .= "VALUES (:username, :token, :tgl_buat, :tgl_exp)";
 
 			$statement = $this->koneksi->prepare($query);
-			$statement->bindParam(':id', $id);	
+			$statement->execute(
+				array(
+					':username' => $data['username'],
+					':token' => $data['token'],
+					':tgl_buat' => $data['tgl_buat'],
+					':tgl_exp' => $data['tgl_exp'],
+				)
+			);
 			
-			$result = $statement->execute();
-
-			return $result;
+			$statement->closeCursor();
 		}
+
+		/**
+		* 
+		*/
+		private function delete_lupa_password($user){
+			$query = "DELETE FROM token_lupa_password WHERE username = :username";
+
+			$statement = $this->koneksi->prepare($query);	
+			$statement->execute(array(':username' => $user));
+
+			$statement->closeCursor();
+		}
+
+		// ======================== mobile ========================= //
+
+			/**
+			* 
+			*/
+			public function getToken_mobile($user){
+				$query = "SELECT t.token, t.tgl_exp FROM token_sub_kas_kecil t ";
+				$query .= "JOIN sub_kas_kecil skc ON skc.id = t.id_sub_kas_kecil ";
+				$query .= "WHERE skc.email = :user";
+
+				$statement = $this->koneksi->prepare($query);
+				$statement->bindParam(':user', $user);
+				$statement->execute();
+				$result = $statement->fetch(PDO::FETCH_ASSOC);
+
+				return $result;
+			}
+
+			/**
+			*
+			*/
+			public function setToken_mobile($data){
+				try{
+					$this->koneksi->beginTransaction();
+					
+					// hapus token lama
+					$this->delete_mobile($data['username']);
+
+					// tambah token baru
+					$this->insert_mobile($data);
+
+					$this->koneksi->commit();
+
+					return true;
+				}
+				catch(PDOException $e){
+					$this->koneksi->rollback();
+					die($e->getMessage());
+					// return false;
+				}
+			}
+
+			/**
+			* 
+			*/
+			private function insert_mobile($data){
+				$query = "INSERT INTO token_mobile (username, token, tgl_buat, tgl_exp) ";
+				$query .= "VALUES (:username, :token, :tgl_buat, :tgl_exp)";
+
+				$statement = $this->koneksi->prepare($query);
+				$statement->execute(
+					array(
+						':username' => $data['username'],
+						':token' => $data['token'],
+						':tgl_buat' => $data['tgl_buat'],
+						':tgl_exp' => $data['tgl_exp'],
+					)
+				);
+				
+				$statement->closeCursor();
+			}
+
+			/**
+			* 
+			*/
+			private function delete_mobile($user){
+				$query = "DELETE FROM token_mobile WHERE username = :username";
+
+				$statement = $this->koneksi->prepare($query);
+				$statement->execute(array(':username' => $user));
+
+				$statement->closeCursor();
+			}
+
+		// ========================================================= //
 
 		/**
 		* 
