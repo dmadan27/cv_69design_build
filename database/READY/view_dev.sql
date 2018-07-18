bec62bd77693d140de73756dea2d4d168de5152c
Fajar Cahyo Prabowo <fajarcahyo15@gmail.com>
2018-07-18 11:13:33 +0700

    pemindahan query sql dari dropbox ke github

---
+++ database/READY/view_dev.sql
@@ -0,0 +1,94 @@
+# Database Sistem Informasi CV 69 Design & Build #
+# Versi 1.0 View #
+
+-- View User
+	-- view untuk semua user yang terdapat di sistem
+	CREATE OR REPLACE VIEW v_user AS
+		SELECT 
+			u.username, kb.nama, u.status, u.level FROM user u JOIN kas_besar kb  ON u.username=kb.email
+		
+		UNION
+		
+		SELECT
+			u.username, kk.nama, u.status, u.level FROM user u JOIN kas_kecil kk  ON u.username=kk.email 
+
+		UNION
+
+		SELECT
+			u.username, skk.nama, u.status, u.level FROM user u JOIN sub_kas_kecil skk  ON u.username=skk.email;				
+
+-- View Kas Besar
+
+-- View Kas Kecil
+
+-- View Sub Kas Kecil
+
+-- View Proyek
+	-- View Get Sub Kas Kecil Proyek
+	CREATE OR REPLACE VIEW v_get_skk_proyek AS
+		SELECT
+			lp.id, lp.id_proyek, skk.id id_skk, skk.nama
+		FROM logistik_proyek lp
+		JOIN sub_kas_kecil skk ON skk.id = lp.id_sub_kas_kecil;
+
+
+-- View Detail Proyek
+
+-- View Logistik Proyek
+	-- skc -> skk belum
+CREATE OR REPLACE VIEW v_proyek_logistik AS
+	SELECT 
+    	p.id id_proyek, p.pemilik, p.tgl, p.pembangunan, p.luas_area, p.alamat, p.kota, p.estimasi, p.total, p.dp, p.cco, p.status,
+        lp.id id_logistik_proyek, skk.id id_sub_kas_kecil, skk.nama, skk.alamat alamat_skk, skk.no_telp, skk.email, skk.foto, skk.saldo, skk.status status_skk
+    FROM proyek p 
+    	
+    JOIN logistik_proyek lp ON lp.id_proyek=p.id
+    JOIN sub_kas_kecil skk ON skk.id=lp.id_sub_kas_kecil;
+
+-- View Bank
+
+-- View Operasional
+CREATE OR REPLACE VIEW v_operasional AS
+	SELECT
+		o.id, o.tgl, o.nama, o.nominal, o.ket, b.id id_bank, b.nama nama_bank
+	FROM operasional o
+	JOIN bank b ON b.id = o.id_bank;
+
+-- View Mutasi Bank
+
+-- View Mutasi Saldo Kas Kecil
+
+-- View Mutasi Saldo Sub Kas Kecil
+
+-- View Pengajuan Sub Kas Kecil
+CREATE OR REPLACE VIEW v_pengajuan_sub_kas_kecil_full AS
+	SELECT
+		pskk.id id_pengajuan, pskk.id_sub_kas_kecil, pskk.id_proyek, pskk.tgl,
+		pskk.total, pskk.dana_disetujui, pskk.status, pskk.status_laporan,
+		dp.id id_detail, dp.nama, dp.jenis, dp.satuan, dp.qty, dp.harga, dp.subtotal,
+		dp.status status_detail, dp.harga_asli, dp.sisa, dp.status_lunas,
+		p.pemilik, p.pembangunan, p.kota
+	FROM pengajuan_sub_kas_kecil pskk
+	JOIN detail_pengajuan_sub_kas_kecil dp ON dp.id_pengajuan = pskk.id
+	JOIN proyek p ON p.id = pskk.id_proyek;
+
+-- View Detail Pengajuan Sub Kas Kecil
+
+-- View Upload Laporan Pengajuan Sub Kas Kecil
+
+-- View Pengajuan Kas Kecil
+CREATE OR REPLACE VIEW v_pengajuan_kas_kecil AS
+	SELECT
+		pkk.id, pkk.nama, pkk.tgl, pkk.total, pkk.status,
+		kk.id id_kas_kecil, kk.nama nama_kas_kecil
+	FROM pengajuan_kas_kecil pkk
+	JOIN kas_kecil kk ON kk.id = pkk.id_kas_kecil;
+
+-- View Detail Pengajuan Kas Kecil
+
+-- View Operasional Proyek
+
+-- View Detail Operasional Proyek
+
+-- 
+
