fcc31f3ba08e8fea9badd68e256f0bc05cf64314
Jaka Pratama <jakapratama204@gmail.com>
2018-07-26 18:36:20 +0700

    PHP EXCEL Modul Sub Kas Kecil

--- app/views/sub_kas_kecil/js/initList.js
+++ app/views/sub_kas_kecil/js/initList.js
@@ -42,6 +42,13 @@ $(document).ready(function(){
         }
     });
 
+    // btn Export
+    $('#exportExcel').on('click', function(){
+        // if(this.value.trim() != "") 
+            window.location.href = BASE_URL+'sub-kas-kecil/export/';
+       
+    });
+
 });
 
 /**
