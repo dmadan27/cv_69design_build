7e1adf3bf656da18e593acc6fdc4eccf5a1ac25f
Jaka Pratama <jakapratama204@gmail.com>
2018-07-31 15:06:35 +0700

    Perubahan library di PhpExcel
    -> size title diperbesar jadi 1000 (karakter)

--- app/library/export_phpexcel/PHPExcel/PHPExcel/Worksheet.php
+++ app/library/export_phpexcel/PHPExcel/PHPExcel/Worksheet.php
@@ -468,7 +468,7 @@ class PHPExcel_Worksheet implements PHPExcel_IComparable
         }
 
         // Maximum 31 characters allowed for sheet title
-        if (PHPExcel_Shared_String::CountCharacters($pValue) > 31) {
+        if (PHPExcel_Shared_String::CountCharacters($pValue) > 1000) {
             throw new PHPExcel_Exception('Maximum 31 characters allowed in sheet title.');
         }
 
