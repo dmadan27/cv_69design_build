bdb48271f2daebd0fb277702ae6b3b0a4a20dcb3
jakapratamaunikom <jakapratama204@gmail.com>
2018-12-26 11:12:14 +0700

    tandai proc fix
    -> on Going

--- app/database/procedure_dev.sql
+++ app/database/procedure_dev.sql
@@ -3,7 +3,7 @@
 
 # ============================ Procedure ============================ #
 
--- Procedure Tambah Data Kas Besar
+-- Procedure Tambah Data Kas Besar {FIX}
 	delimiter //
 	CREATE PROCEDURE tambah_kas_besar(
 		in id_param varchar(10),
@@ -38,7 +38,7 @@
 	-- tanpa prosedur
 
 
--- Procedure Hapus Data Kas Besar
+-- Procedure Hapus Data Kas Besar {FIX}
 	delimiter //
 	CREATE PROCEDURE hapus_kas_besar(
 		in id_param varchar(10)
@@ -110,7 +110,7 @@
 	delimiter ;
 
 
--- Procedure Tambah Data Kas Kecil
+-- Procedure Tambah Data Kas Kecil {FIX}
 	delimiter //
 	CREATE PROCEDURE tambah_kas_kecil(
 		in id_param varchar(10),
