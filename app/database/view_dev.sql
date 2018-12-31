5cbb44fc3353123f40cb6eb3d1eb8616b0a78c65
Jaka Pratama <jakapratama204@gmail.com>
2018-12-31 21:52:10 +0700

    nambah view history distributor

--- app/database/view_dev.sql
+++ app/database/view_dev.sql
@@ -174,3 +174,19 @@ SELECT
 		JOIN	
 			distributor d
 				ON opr.id_distributor = d.id
+
+
+/*  History Distributor di menu 'DATA DISTRIBUTOR '
+		
+		Status : harus masih di review
+*/
+
+CREATE OR REPLACE VIEW v_history_distributor AS
+		SELECT d.id , d.nama NAMA_DISTRIBUTOR, d.pemilik PEMILIK_DISTRIBUTOR, 
+			opr.id  ID_OPERASIONAL_PROYEK, opr.nama NAMA_KEBUTUHAN
+		FROM	
+			distributor d 
+				JOIN
+					operasional_proyek opr
+						ON d.id = 	opr.id_distributor
+						WHERE d.id = opr.id_distributor
