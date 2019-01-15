45854e93351fd38cd6dbcb35ad3a924bcb48518d
Ramadan Saputra <rarasta27@gmail.com>
2019-01-15 21:15:59 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -12,7 +12,7 @@
 		define('BASE_URL', 'http://localhost/cv_69design_build/'); // isi path dari web
 		define('SITE_URL', BASE_URL.'index.php/'); // hilangkan index.php atau komentari SITE_URL jika sudah memakai .htaccess
 		define('DEFAULT_CONTROLLER', 'home'); // default controller yg diakses pertama kali
-		define('VERSION', 'Alpha v0.1');
+		define('VERSION', 'Beta v1.0');
 
 		// config database
 		define('DB_HOST', 'localhost'); // host db
@@ -21,28 +21,26 @@
 		define('DB_NAME', '69design-build'); // db yang digunakan
 	}
 	else if(STATUS_DEV == 'LIVE'){
-		define('BASE_URL', 'https://69design-build.lordraze.com/'); // isi path dari web
-		define('SITE_URL', BASE_URL.'index.php/'); // hilangkan index.php atau komentari SITE_URL jika sudah memakai .htaccess
-		define('DEFAULT_CONTROLLER', 'home'); // default controller yg diakses pertama kali
-		define('VERSION', 'Alpha v0.1');
+		define('BASE_URL', 'https://dev.69designbuild.com/v1/web/'); 
+		define('SITE_URL', BASE_URL.'index.php/'); 
+		define('DEFAULT_CONTROLLER', 'home');
+		define('VERSION', 'Beta v1.0');
 
-		// config database
-		define('DB_HOST', 'localhost'); // host db
-		define('DB_USERNAME', 'lordraze_full'); // username db
-		define('DB_PASSWORD', 'VixyBlack27'); // password db
-		define('DB_NAME', 'lordraze_69_design_build'); // db yang digunakan
+		define('DB_HOST', 'localhost');
+		define('DB_USERNAME', 'designbu_full');
+		define('DB_PASSWORD', '69db69db69db');
+		define('DB_NAME', 'designbu_69design_build.sql');
 	}
 	else if(STATUS_DEV == 'PRODUCTION'){
-		define('BASE_URL', 'https://admin.69design-build.com/'); // isi path dari web
-		define('SITE_URL', BASE_URL.'index.php/'); // hilangkan index.php atau komentari SITE_URL jika sudah memakai .htaccess
-		define('DEFAULT_CONTROLLER', 'home'); // default controller yg diakses pertama kali
-		define('VERSION', '');
+		define('BASE_URL', 'https://system.69design-build.com/');
+		define('SITE_URL', BASE_URL.'index.php/');
+		define('DEFAULT_CONTROLLER', 'home');
+		define('VERSION', 'v1.0');
 
-		// config database
-		define('DB_HOST', 'localhost'); // host db
-		define('DB_USERNAME', 'admin'); // username db
-		define('DB_PASSWORD', ''); // password db
-		define('DB_NAME', '69design-build'); // db yang digunakan
+		define('DB_HOST', 'localhost');
+		define('DB_USERNAME', 'designbu_full');
+		define('DB_PASSWORD', '69db69db69db');
+		define('DB_NAME', 'designbu_69design_build');
 	}
 	else die(json_encode(
 		array(
