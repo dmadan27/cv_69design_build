9517d697f081dbdb2e24c4693dce482b116c98e1
Ramadan Saputra <rarasta27@gmail.com>
2019-01-20 23:14:50 +0700

    Update config.php
    => Penambahan untuk testing

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,5 @@
 <?php
-	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
+	Defined("BASE_PATH") or die(ACCESS_DENIED);
 	
 	// KEY FIREBASE NOTIFICATION
 	define(
@@ -7,7 +7,7 @@
 		'key=AAAAlOkhnSY:APA91bEqHj4gSO-lEqIeQz4g0ABtrhnSa6w8zDWMlXno50jkyJt5VwrfiC91uXv55yM560VyV4QaL9JG7XBktaX1IeMPJeNB7wDaFtI_Z3d2Qk6tnUeV0lYVxtvbF94fw_7rQqk8foLO'
 	);
 
-	if(STATUS_DEV == 'DEVELOPMENT'){
+	if(STATUS_DEV == 'DEVELOPMENT'){ // local
 		// config base url
 		define('BASE_URL', 'http://localhost/cv_69design_build/'); // isi path dari web
 		define('SITE_URL', BASE_URL.'index.php/'); // hilangkan index.php atau komentari SITE_URL jika sudah memakai .htaccess
@@ -20,7 +20,7 @@
 		define('DB_PASSWORD', ''); // password db
 		define('DB_NAME', '69design-build'); // db yang digunakan
 	}
-	else if(STATUS_DEV == 'LIVE'){
+	else if(STATUS_DEV == 'LIVE'){ // dev live
 		define('BASE_URL', 'https://dev.69designbuild.com/v1/web/'); 
 		define('SITE_URL', BASE_URL.'index.php/'); 
 		define('DEFAULT_CONTROLLER', 'home');
@@ -31,7 +31,18 @@
 		define('DB_PASSWORD', '69db69db69db');
 		define('DB_NAME', 'designbu_69design_build.sql');
 	}
-	else if(STATUS_DEV == 'PRODUCTION'){
+	else if(STATUS_DEV == 'TESTING'){ // testing before production
+		define('BASE_URL', 'https://test.69designbuild.com/'); 
+		define('SITE_URL', BASE_URL.'index.php/'); 
+		define('DEFAULT_CONTROLLER', 'home');
+		define('VERSION', 'v1.0');
+
+		define('DB_HOST', 'localhost');
+		define('DB_USERNAME', 'designbu_full');
+		define('DB_PASSWORD', '69db69db69db');
+		define('DB_NAME', 'designbu_69design_build_testing');
+	}
+	else if(STATUS_DEV == 'PRODUCTION'){ // production
 		define('BASE_URL', 'https://system.69design-build.com/');
 		define('SITE_URL', BASE_URL.'index.php/');
 		define('DEFAULT_CONTROLLER', 'home');
@@ -42,11 +53,6 @@
 		define('DB_PASSWORD', '69db69db69db');
 		define('DB_NAME', 'designbu_69design_build');
 	}
-	else die(json_encode(
-		array(
-			'success' => false,
-			'message' => 'Access Denied'
-		)
-	));
+	else die(ACCESS_DENIED);
 
 		
