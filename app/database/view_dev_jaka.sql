3eae1fbd7ecd8db2094e58ce25748211d2b101ec
Ramadan Saputra <rarasta27@gmail.com>
2019-01-20 23:13:29 +0700

    Penghapusan beberapa file tidak penting

--- app/database/view_dev_jaka.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE VIEW v_pengajuan_kas_kecil 
-	AS
-		SELECT pkk.id, pkk.id_kas_kecil, kk.nama, pkk.tgl,
-			pkk.nama pkk.total, pkk.status
-		FROM pengajuan_kas_kecil pkk 
-			JOIN
-				kas_kecil kk ON pkk.id_kas_kecil = kk.id;
