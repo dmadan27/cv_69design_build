f9dfed8165c2a6e5b3dae939e1fc4009a8fcfa34
Fajar Cahyo Prabowo <fajarcahyo15@gmail.com>
2019-02-22 19:50:09 +0700

    perbaikan sendNotif ke android

--- app/library/Helper.class.php
+++ app/library/Helper.class.php
@@ -307,6 +307,8 @@
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_POST, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, '3'); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
