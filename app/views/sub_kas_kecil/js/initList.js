4dadb7d9e8043f7a91c162645d596ec7398ad10e
fajarcahyo15 <fajarcahyo15@gmail.com>
2019-03-22 14:38:43 +0700

    update export data sub-kas-kecil

--- app/views/sub_kas_kecil/js/initList.js
+++ app/views/sub_kas_kecil/js/initList.js
@@ -1,5 +1,5 @@
 var skcTable = $("#skcTable").DataTable({
-    "language" : {
+    "language": {
         "lengthMenu": "Tampilkan _MENU_ data/page",
         "zeroRecords": "Data Tidak Ada",
         "info": "Menampilkan _START_ s.d _END_ dari _TOTAL_ data",
@@ -14,65 +14,64 @@ var skcTable = $("#skcTable").DataTable({
             "previous": "Sebelumnya"
         }
     },
-    "lengthMenu": [ 10, 25, 75, 100 ],
+    "lengthMenu": [10, 25, 75, 100],
     "pageLength": 10,
     order: [],
     processing: true,
     serverSide: true,
     ajax: {
-        url: BASE_URL+"sub-kas-kecil/get-list/",
+        url: BASE_URL + "sub-kas-kecil/get-list/",
         type: 'POST',
         data: {
         }
     },
     "columnDefs": [
         {
-            "targets":[0, 7], // disable order di kolom 1 dan 3
-            "orderable":false,
+            "targets": [0, 7], // disable order di kolom 1 dan 3
+            "orderable": false,
         }
     ],
-    createdRow: function(row, data, dataIndex){
-        if(data[0]) { $('td:eq(0)', row).addClass('text-right'); }
-        if(data[5]) { $('td:eq(5)', row).addClass('text-right'); }
-        if(data[6]) { $('td:eq(6)', row).addClass('text-center'); }
-        if($(data[6]).text().toLowerCase() == "nonaktif") $(row).addClass('danger');
+    createdRow: function (row, data, dataIndex) {
+        if (data[0]) { $('td:eq(0)', row).addClass('text-right'); }
+        if (data[5]) { $('td:eq(5)', row).addClass('text-right'); }
+        if (data[6]) { $('td:eq(6)', row).addClass('text-center'); }
+        if ($(data[6]).text().toLowerCase() == "nonaktif") $(row).addClass('danger');
     }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // btn Export
-    $('#exportExcel').on('click', function(){
+    $('#exportExcel').on('click', function () {
         // if(this.value.trim() != "") 
-            window.location.href = BASE_URL+'sub-kas-kecil/export/';
-       
+        window.location.href = BASE_URL + 'export/sub-kas-kecil';
     });
 
     // event on click refresh table
-    $('#refreshTable').on('click', function() {
+    $('#refreshTable').on('click', function () {
         console.log('Button Refresh Table Sub Kas Kecil clicked...');
         refreshTable(skcTable, $(this));
     });
 
     // auto refresh every 1 minutes
-    setInterval( function () {
+    setInterval(function () {
         console.log('%cAutomatically refresh table..', 'color: blue; font-style: italic');
         skcTable.ajax.reload(null, false);
-    }, 60000 );
+    }, 60000);
 
 });
 
 /**
 *
 */
-function getView(id){
-	window.location.href = BASE_URL+'sub-kas-kecil/detail/'+id.toLowerCase();
+function getView(id) {
+    window.location.href = BASE_URL + 'sub-kas-kecil/detail/' + id.toLowerCase();
 }
 
 /**
 *
 */
-function getDelete(id){
+function getDelete(id) {
 
     swal({
         title: "Pesan Konfirmasi",
@@ -83,26 +82,26 @@ function getDelete(id){
         confirmButtonText: "Ya, Hapus!",
         cancelButtonText: "Batal",
         closeOnConfirm: false,
-    }, function(){
+    }, function () {
         $.ajax({
-            url: BASE_URL+'sub-kas-kecil/delete/'+id.toLowerCase(),
+            url: BASE_URL + 'sub-kas-kecil/delete/' + id.toLowerCase(),
             type: 'post',
             dataType: 'json',
             data: {},
-            beforeSend: function(){
+            beforeSend: function () {
             },
-            success: function(response){
+            success: function (response) {
                 console.log(response);
-                if(response.success){
+                if (response.success) {
                     swal("Pesan Berhasil", "Data Berhasil Dihapus", "success");
                     $("#skcTable").DataTable().ajax.reload();
                 }
             },
-            error: function (jqXHR, textStatus, errorThrown){ // error handling
+            error: function (jqXHR, textStatus, errorThrown) { // error handling
                 console.log(jqXHR, textStatus, errorThrown);
                 swal("Pesan Gagal", "Terjadi Kesalahan Teknis, Silahkan Coba Kembali", "error");
             }
         })
     });
-	
+
 }
