efcacb9598f668fb70ab0b5f2df850df7d78c51b
Fajar Cahyo Prabowo <fajarcahyo15@gmail.com>
2019-06-09 08:07:21 +0700

    pembaruan form export start end date

--- app/views/form_export/form_start_end_date.php
+++ app/views/form_export/form_start_end_date.php
@@ -1,6 +1,6 @@
 <?php Defined("BASE_PATH") or die(ACCESS_DENIED); ?>
 
-<div class="modal fade" id="modalExport">
+<div class="modal fade" id="modal-export-start-end-date">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<!-- header modal -->
@@ -8,11 +8,11 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
-                <h4 class="modal-title">Export</h4>
+                <h4 class="modal-title modal-export-title">Export</h4>
 			</div>
-			<form id="form_export" role="form">
-				<input type="hidden" id="id_for_export">
-                <input type="hidden" id="exportType">
+			<form id="form-export-start-end-date" role="form">
+				<input type="hidden" id="export-data">
+
 				<!-- body modal -->
 				<div class="modal-body">
 
@@ -20,9 +20,9 @@
 					<div class="form-group field-tgl_export has-feedback">
 						<label>Tanggal Export</label>
 						<div class="input-group">
-							<input type="text" id="tgl_awal" class="form-control field datepicker" placeholder="Masukkan Tanggal Awal">
+							<input type="text" id="tgl-awal" class="form-control field datepicker" placeholder="Masukkan Tanggal Awal" autocomplete="off">
 							<span class="input-group-addon">s.d</span>
-							<input type="text" id="tgl_akhir" class="form-control field datepicker" placeholder="Masukkan Tanggal Akhir">
+							<input type="text" id="tgl-akhir" class="form-control field datepicker" placeholder="Masukkan Tanggal Akhir" autocomplete="off">
 						</div>
 						<span class="help-block small pesan pesan-tgl_export"></span>
 					</div>
@@ -30,8 +30,8 @@
 				</div>
 				<!-- modal footer -->
 				<div class="modal-footer">
-					<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Kembali</button>
-	                <button type="submit" id="submit_export" class="btn btn-primary">Export</button>
+					<button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal">Kembali</button>
+	                <button type="submit" id="btn-export-start-end-date" class="btn btn-primary btn-flat">Export</button>
 				</div>
 			</form>
 		</div>
---
+++ assets/js/library/export.js
@@ -0,0 +1,58 @@
+/**
+ * Object Export Data.
+ */
+const Export = {
+    /**
+     * Memanggil url export excel.
+     * 
+     * @param {FormData} body Object berisi tanggal atau range tanggal data yang ingin diexport (opsional).
+     * @param {string} method Nama method export excel yang ingin direquest untuk mendapatkan file excel.
+     * @param {string} id String id yang diperlukan untuk melakukan export (opsional).
+     */
+    'excel': async ({ method, id = "", body = new FormData() }) => {
+        const res = await fetch(BASE_URL + 'export/' + method + "/" + id, {
+            method: "POST",
+            body: body,
+        });
+
+        if (res.status == 200) {
+            const data = await res.json();
+            if (data.success) {
+                let $a = $("<a>");
+                $a.attr("href", data.file);
+                $("body").append($a);
+                $a.attr("download", data.filename);
+                $a[0].click();
+                $a.remove();
+            } else {
+                throw new InfoException(data.message);
+            }
+        } else {
+            throw ("Tidak dapat terhubung ke server (" + res.status + ").");
+        }
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: BASE_URL + 'export/' + method + "/" + id,
+        //     data: body,
+        //     dataType: "JSON",
+        //     success: (response) => {
+        //         console.log('%cResponse getExport: ', 'color: blue; font-weight: bold', response);
+        //         if (response.success) {
+        //             let $a = $("<a>");
+        //             $a.attr("href", response.file);
+        //             $("body").append($a);
+        //             $a.attr("download", response.filename);
+        //             $a[0].click();
+        //             $a.remove();
+        //         } else {
+        //             throw new InfoException(response.message);
+        //         }
+        //     },
+        //     error: (jqXHR, textStatus, errorThrown) => { // error handling
+        //         console.log('%cResponse Error Export ' + method, 'color: red; font-weight: bold', { jqXHR, textStatus, errorThrown });
+        //         throw ("Terjadi Kesalahan Teknis, Silahkan Coba Kembali");
+        //     },
+        // });
+    }
+}
