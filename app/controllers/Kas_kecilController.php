6192e9704c487253e6de624d0273a4834a6f245b
Jaka Pratama <jakapratama204@gmail.com>
2019-06-26 18:57:30 +0700

    restrict edit, delete  kas kecil for owner

--- app/controllers/Kas_kecilController.php
+++ app/controllers/Kas_kecilController.php
@@ -93,7 +93,16 @@
 					$aksiEdit = '<button onclick="getEdit('."'".$row["id"]."'".')" type="button" class="btn btn-sm btn-success btn-flat" title="Edit Data"><i class="fa fa-pencil"></i></button>';
 					$aksiHapus = '<button onclick="getDelete('."'".$row["id"]."'".')" type="button" class="btn btn-sm btn-danger btn-flat" title="Hapus Data"><i class="fa fa-trash"></i></button>';
 					
-					$aksi = '<div class="btn-group">'.$aksiDetail.$aksiEdit.$aksiHapus.'</div>';
+
+					if($_SESSION['sess_level'] === 'KAS BESAR') {
+						$aksi = '<div class="btn-group">'.$aksiDetail.$aksiEdit.$aksiHapus.'</div>';
+					}
+					else if($_SESSION['sess_level'] === 'OWNER') {
+						$aksi = '<div class="btn-group">'.$aksiDetail.'</div>';
+					}
+					else { $aksi = ''; }
+
+					
 					
 					$dataRow = array();
 					$dataRow[] = $no_urut;
