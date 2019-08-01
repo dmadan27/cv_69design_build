7fe73da81e3c65cb7a87d2c720ab77321e7f03ed
Romadan Saputra <dev.rarasta27@gmail.com>
2019-08-01 10:35:16 +0700

    Update 9_seeder_increment.sql
    Perbaikan seeder increment yg tertukar khusunya di KK, KB, dan SKC

--- databases/dev/seeder/9_seeder_increment.sql
+++ databases/dev/seeder/9_seeder_increment.sql
@@ -7,8 +7,8 @@ INSERT INTO increment (id, menu_id, mask, last_increment) VALUES
 (4, 6, 'OPR-yyyy-0001', 0),
 (5, 7, 'PKK-yyyy-0001', 0),
 (6, 8, 'PSKK-yyyy-0001', 0),
-(7, 10, 'KB-001', 4),
-(8, 11, 'KK-001', 6),
-(9, 12, 'SKK-001', 6);
+(7, 11, 'KB-001', 4),
+(8, 12, 'KK-001', 6),
+(9, 13, 'SKK-001', 6);
 
 # End Seeder Increment #
